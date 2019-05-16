2f7a7c46530a3885352968b116c2ffeb152d9327
Maded Batara III <mnbatara3@gmail.com>
2019-05-16 21:49:11 +0800

    added hand comments

--- hand.h
+++ hand.h
@@ -1,35 +1,46 @@
+//
+// Classes for implementing tappable appendages and appendage types.
+//
+// @author Maded Batara III (blubits)
+// @date 2019-05-16
+
 #include <ostream>
 #include <string>
 
+// Represents the appendages of a certain player.
+// The Appendage class is a semi-abstract class; appendage subclasses
+// (e.g. hands and feet) are expected to implement add_digits() and is_dead(),
+// as the logic for these functions will vary across appendage types.
 class Appendage {
    protected:
-    int max;
     int raised;
+    int max;
 
    public:
     Appendage();
-    Appendage(int max, int raised);
+    Appendage(int raised, int max);
     std::string get_name() const;
-    int get_max() const;
     int get_raised() const;
-    void set_raised(int raised);
+    int get_max() const;
 
-    // Abstract functions; override in subclasses
-    virtual void tap(const Appendage& other) = 0;
+    // Add additional raised digits to the appendage. Can only
+    // be done as long as the appendage is still alive.
+    virtual void add_digits(int digits) = 0;
+
+    // Checks if the appendage is dead.
     virtual bool is_dead() const = 0;
 
+    // Directs program state to an output stream.
     friend std::ostream& operator<<(std::ostream& os, const Appendage& dt);
 };
 
-Appendage::Appendage() : max(5), raised(1){};
+Appendage::Appendage() : raised(1), max(5){};
 
-Appendage::Appendage(int max, int raised) : max(max), raised(raised){};
+Appendage::Appendage(int raised, int max) : raised(raised), max(max){};
 
 int Appendage::get_max() const { return max; }
 
 int Appendage::get_raised() const { return raised; }
-
-void Appendage::set_raised(int raised) { this->raised = raised; }
 
 std::ostream& operator<<(std::ostream& os, const Appendage& dt) {
     if (dt.is_dead()) {
@@ -40,30 +51,37 @@ std::ostream& operator<<(std::ostream& os, const Appendage& dt) {
     return os;
 }
 
+// A player's hands.
+// Note that a player's hand obeys standard rules of Chopsticks; fingers
+// wrap around, and the hand remains alive until all fingers are raised.
 class Hand : public Appendage {
    public:
     using Appendage::Appendage;
-    void tap(const Appendage& other) override;
+    void add_digits(int digits) override;
     bool is_dead() const override;
 };
 
-void Hand::tap(const Appendage& other) {
+void Hand::add_digits(int digits) {
     if (is_dead()) return;
     raised = (raised - 1 + other.get_raised()) % max + 1;
 }
 
 bool Hand::is_dead() const { return raised == max; }
 
+// A player's feet.
+// Toes in this implementation of the game do not wrap around and are simply
+// added until all possible toes have been raised. Note that a player skips
+// a turn if one of his feet dies.
 class Foot : public Appendage {
    public:
     using Appendage::Appendage;
-    void tap(const Appendage& other) override;
+    void add_digits(int digits) override;
     bool is_dead() const override;
 };
 
-void Foot::tap(const Appendage& other) {
+void Foot::add_digits(int digits) {
     if (is_dead()) return;
-    raised += other.get_raised();
+    raised += digits;
 }
 
 bool Foot::is_dead() const { return raised >= max; }
