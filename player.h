6e36ecf01730fedc05cae36ef22feb9ba21b0bc5
jetsalinas <joseenrico.salinas@gmail.com>
2019-05-18 23:47:11 +0800

    Added header guards. Added getter for appendages

--- player.h
+++ player.h
@@ -4,11 +4,14 @@
 // @author Maded Batara III (blubits)
 // @date 2019-05-16
 
+#ifndef PLAYER_H
+#define PLAYER_H
+
 #include <ostream>
 #include <string>
 #include <vector>
+#include "hand.h"
 #include "playerinfo.h"
-#include "tapper.h"
 
 // Represents a game player.
 class Player {
@@ -21,20 +24,21 @@ class Player {
     std::vector<Hand *> hands;
     std::vector<Foot *> feet;
 
-    bool turn_skipped = false;
+    bool turn = true;
 
    public:
-    Player(int player_team, int player_order, char player_type,
+    Player(int player_order, char player_type,
            const PlayerInfo *player_info);
-    Hand &get_hand(int i);
-    Foot &get_foot(int i);
+    Hand *get_hand(int i);
+    Foot *get_foot(int i);
     void distribute_hands(std::vector<int> input);
     void distribute_feet(std::vector<int> input);
     bool is_dead();
+    bool has_turn();
+    void set_turn(bool t);
 
     // Abstract functions: override in subclasses
-    virtual void tap(Player &other, Tapper &tapper);
-    virtual void get_tapped(Tapper &tapper);
+    // virtual void get_tapped();
 
     friend std::ostream &operator<<(std::ostream &os, const Player &dt);
     std::string to_string();
@@ -46,11 +50,19 @@ Player::Player(int player_order, char player_type,
       player_type(player_type),
       player_info(player_info) {
     for (int i = 0; i < player_info->num_hands; i++) {
-        hands.push_back(new Hand(player_info->num_fingers, 0));
+        hands.push_back(new Hand(1, player_info->num_fingers));
     }
     for (int i = 0; i < player_info->num_feet; i++) {
-        feet.push_back(new Foot(player_info->num_toes, 0));
+        feet.push_back(new Foot(1, player_info->num_toes));
     }
+}
+
+Hand *Player::get_hand(int i) {
+    return hands[i];
+}
+
+Foot *Player::get_foot(int i) {
+    return feet[i];
 }
 
 void Player::distribute_hands(std::vector<int> input) {
@@ -79,11 +91,19 @@ bool Player::is_dead() {
 
 std::ostream &operator<<(std::ostream &os, const Player &dt) {
     os << "P" << dt.player_order << dt.player_type << " (";
-    for (auto &h : dt.hands) os << h;
+    for (auto &h : dt.hands) os << (*h);
     os << ":";
-    for (auto &f : dt.feet) os << f;
+    for (auto &f : dt.feet) os << (*f);
     os << ")";
     return os;
+}
+
+bool Player::has_turn() {
+    return turn;
+}
+
+void Player::set_turn(bool t) {
+    turn = t;
 }
 
 class Human : public Player {
@@ -93,8 +113,6 @@ class Human : public Player {
 };
 
 Human::Human(int player_order) : Player(player_order, 'h', &HUMAN_INFO){};
-
-void Human::get_tapped_by(Player &player, Appendage &appendage) { ; }
 
 class Alien : public Player {
    public:
@@ -128,3 +146,5 @@ Player *PlayerFactory::make_player(int player_order, std::string player_type) {
     };
     return nullptr;
 };
+
+#endif
