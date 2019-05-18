fc6f9d4791615aa0a5c0c75123fd3ce64c9fdb99
jetsalinas <joseenrico.salinas@gmail.com>
2019-05-18 23:47:21 +0800

    Added header guards

--- playerinfo.h
+++ playerinfo.h
@@ -1,6 +1,9 @@
 // @author Maded Batara III (blubits)
 // @date 2019-05-16
 
+#ifndef PLAYERINFO_H
+#define PLAYERINFO_H
+
 // Info on a player's appendages.
 struct PlayerInfo {
     int num_hands;
@@ -10,8 +13,8 @@ struct PlayerInfo {
     PlayerInfo(int num_hands, int num_feet, int num_fingers, int num_toes);
 };
 
-PlayerInfo::PlayerInfo(int num_hands, int num_feet, int fingers_max,
-                       int toes_max)
+PlayerInfo::PlayerInfo(int num_hands, int num_feet, int num_fingers,
+                       int num_toes)
     : num_hands(num_hands),
       num_feet(num_feet),
       num_fingers(num_fingers),
@@ -21,3 +24,5 @@ const PlayerInfo HUMAN_INFO(2, 2, 5, 5);
 const PlayerInfo ALIEN_INFO(4, 2, 3, 2);
 const PlayerInfo ZOMBIE_INFO(1, 0, 4, 0);
 const PlayerInfo DOGGO_INFO(0, 4, 0, 4);
+
+#endif
