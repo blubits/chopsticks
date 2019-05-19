29cfe5700417cebdb6d6ba5b51188e0bc81bf32f
jetsalinas <joseenrico.salinas@gmail.com>
2019-05-19 09:29:39 +0800

    Added num_actions to playerinfo

--- playerinfo.h
+++ playerinfo.h
@@ -10,19 +10,21 @@ struct PlayerInfo {
     int num_feet;
     int num_fingers;
     int num_toes;
-    PlayerInfo(int num_hands, int num_feet, int num_fingers, int num_toes);
+    int num_actions;
+    PlayerInfo(int num_hands, int num_feet, int num_fingers, int num_toes, int num_actions);
 };
 
 PlayerInfo::PlayerInfo(int num_hands, int num_feet, int num_fingers,
-                       int num_toes)
+                       int num_toes, int num_actions)
     : num_hands(num_hands),
       num_feet(num_feet),
       num_fingers(num_fingers),
-      num_toes(num_toes){};
+      num_toes(num_toes),
+      num_actions(num_actions){};
 
-const PlayerInfo HUMAN_INFO(2, 2, 5, 5);
-const PlayerInfo ALIEN_INFO(4, 2, 3, 2);
-const PlayerInfo ZOMBIE_INFO(1, 0, 4, 0);
-const PlayerInfo DOGGO_INFO(0, 4, 0, 4);
+const PlayerInfo HUMAN_INFO(2, 2, 5, 5, 1);
+const PlayerInfo ALIEN_INFO(4, 2, 3, 2, 1);
+const PlayerInfo ZOMBIE_INFO(1, 0, 4, 0, 2);
+const PlayerInfo DOGGO_INFO(0, 4, 0, 4, 1);
 
 #endif
