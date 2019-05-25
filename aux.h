f5e2ae2aac4661edd205b4b3ab4362418e44a70a
jetsalinas <joseenrico.salinas@gmail.com>
2019-05-25 21:12:44 +0800

    Added auxilliary header file

---
+++ aux.h
@@ -0,0 +1,25 @@
+#include "ctype.h"
+#include "stdlib.h"
+
+#ifndef AUX_h
+#define AUX_H
+
+bool is_valid_port(char *port) {
+    int i = 0;
+    while (port[i]) {
+        if (!isdigit(port[i])) {
+            return false;
+        }
+    }
+
+    long long int port_long;
+    port_long = atol(port);
+
+    if (port_long >= 1024 && port_long <= 65535) {
+        return true;
+    }
+
+    return false;
+}
+
+#endif
