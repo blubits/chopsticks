cf5460140a584e2ba45ce4450f20b51fa9a0bf7f
jetsalinas <joseenrico.salinas@gmail.com>
2019-05-25 21:19:41 +0800

    Added protocol codes

---
+++ protocol.h
@@ -0,0 +1,8 @@
+enum class CODES : int {
+    NEW_BROADCAST = 0,
+    REQUEST_NEW_INPUT = 100,
+    NEW_INPUT = 101,
+    GAME_END = 200,
+    TERMINATE_CONNECTION = 400,
+    PLAYER_DISCONNECT = 401
+};
