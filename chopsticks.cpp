eb4edec383876b324ed9a1f3d499584c6eeb48da
jetsalinas <joseenrico.salinas@gmail.com>
2019-05-25 21:07:26 +0800

    Removed old code in chopsticks.cpp

--- chopsticks.cpp
+++ chopsticks.cpp
@@ -1,46 +1,54 @@
 // CS12 S - Batara, Salinas
 
 #include <iostream>
-#include <string>
-#include <vector>
+#include "aux.h"
+#include "ctype.h"
 #include "game.h"
+#include "stdlib.h"
 
 using namespace std;
 
-int main() {
-    int p;
-    int t;
-    cin >> p >> t;
+int main(int argc, char *argv) {
+    if (argc == 1) {
+        // Attempt to create a server
 
-    Game g;
-    for (int i = 0; i < t; i++) {
-        g.push_team();
+    } else if (argc == 2) {
+        // Attempt to create a client
     }
 
-    for (int i = 1; i < p + 1; i++) {
-        string player_type;
-        int player_team;
-        cin >> player_type >> player_team;
-        g.push_player(player_team - 1, player_type);
-    }
+    // int p;
+    // int t;
+    // cin >> p >> t;
 
-    cin.ignore();
-    g.start_game();
-    cout << g << endl;
+    // Game g;
+    // for (int i = 0; i < t; i++) {
+    //     g.push_team();
+    // }
 
-    int i = 1;
-    string command;
-    while (getline(cin, command)) {
-        if (DEBUG) {
-            cout << "MAIN:main() Turn #" << i << " | " << command << endl;
-            i++;
-        }
+    // for (int i = 1; i < p + 1; i++) {
+    //     string player_type;
+    //     int player_team;
+    //     cin >> player_type >> player_team;
+    //     g.push_player(player_team - 1, player_type);
+    // }
 
-        g.move(command);
-        cout << g << endl;
-        if (!g.is_ongoing()) {
-            cout << "Team " << g.who_won() + 1 << " wins!" << endl;
-            break;
-        }
-    }
+    // cin.ignore();
+    // g.start_game();
+    // cout << g << endl;
+
+    // int i = 1;
+    // string command;
+    // while (getline(cin, command)) {
+    //     if (DEBUG) {
+    //         cout << "MAIN:main() Turn #" << i << " | " << command << endl;
+    //         i++;
+    //     }
+
+    //     g.move(command);
+    //     cout << g << endl;
+    //     if (!g.is_ongoing()) {
+    //         cout << "Team " << g.who_won() + 1 << " wins!" << endl;
+    //         break;
+    //     }
+    // }
 }
